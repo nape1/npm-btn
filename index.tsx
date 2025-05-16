1412c8893d036ce9a1a96829b62522cab1f241db
nape1 <najinshah.pearson@gmail.com>
2025-05-17 03:48:01 +0530

    classname corrected

--- index.tsx
+++ index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 interface NpmBtnProps {
   text: string;
-  className?: string;
+  className: string;
 }
 
-const NpmBtn: React.FC<NpmBtnProps> = ({ text, className }) => {
+const NpmBtn: React.FC<NpmBtnProps> = ({ text, className='' }) => {
   return (
     <button className={`npm-btn btn-${className}`}>{text}</button>
   );
