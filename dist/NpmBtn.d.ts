33a898ad1ccf7598d786492e5cfd1e7c34a2dfbc
nape1 <najinshah.pearson@gmail.com>
2025-05-17 03:04:19 +0530

    btn build

--- dist/NpmBtn.d.ts
+++ dist/NpmBtn.d.ts
@@ -1,6 +1,7 @@
 import React from 'react';
 interface NpmBtnProps {
     text: string;
+    className?: string;
 }
 declare const NpmBtn: React.FC<NpmBtnProps>;
 export default NpmBtn;
--- dist/NpmBtn.js
+++ dist/NpmBtn.js
@@ -1,6 +1,6 @@
 import React from 'react';
 var NpmBtn = function (_a) {
-    var text = _a.text;
-    return (React.createElement("button", null, text));
+    var text = _a.text, className = _a.className;
+    return (React.createElement("button", { className: "npm-btn btn-".concat(className) }, text));
 };
 export default NpmBtn;
