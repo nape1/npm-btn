b0b3328578053f8e3f423e53b7f3af4a1116d75a
n. <najinshah.pearson@gmail.com>
2025-05-17 04:02:49 +0530

    minor vesion release (#3)
    * classname corrected
    
    * workflow file changed
    
    * 1.0.2
    
    * on click added
    
    * 1.1.0

--- index.tsx
+++ index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 interface NpmBtnProps {
   text: string;
-  className?: string;
+  className: string;
 }
 
-const NpmBtn: React.FC<NpmBtnProps> = ({ text, className }) => {
+const NpmBtn: React.FC<NpmBtnProps> = ({ text, className='' }) => {
   return (
-    <button className={`npm-btn btn-${className}`}>{text}</button>
+    <button className={`npm-btn btn-${className}`} onClick={() => console.log('npm-btn clicked')}>{text}</button>
   );
 };
 
